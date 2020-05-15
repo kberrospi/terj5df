fad19be9d160fb7aed0aa815d23d6608fa2408bd
Keivin <keivinbr13@gmail.com>
2020-05-14 20:47:57 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      usr: [],
+      newName: '',
+      newLastNam: ''
+    }
+  }
+  
+
+  crt = (e) =>{
+    e.preventDefault();
+    if(!this.state.newName || !this.state.newLastNam){
+      alert("Por favot llena los campos")
+    }else{
+      e.preventDefault();
+      this.setState({
+        usr: this.state.usr.concat({
+          name: this.state.newName,
+          lastname: this.state.newLastNam,
+        }),
+        newName: '',
+        newLastNam: '',
+      });
+    }
+   
+  }
+
+  changN = (e) => {
+    this.setState({
+      newName: e.target.value,
+    });
+    
+  }
+
+  changLn = (e) => {
+    this.setState({
+      newLastNam: e.target.value,
+    });
+  }
+  
+
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.crt}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  name="first-name" 
+                  value={this.state.newName}
+                  onChange={this.changN}
+                  />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  name="last-name" 
+                  value={this.state.newLastNam}
+                  onChange={this.changLn}
+                  />
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+                <button type="submit" 
+                className="btn btn-primary"
+                >Agregar Invitado</button>
               </div>
             </form>
 
@@ -28,6 +85,12 @@ class App extends Component {
                   <th>Nombre</th>
                   <th>Apellido</th>
                 </tr>
+                  {this.state.usr.map((usr, ind)=>
+                    <tr key={ind}> 
+                      <td >{usr.name}</td>
+                      <td >{usr.lastname}</td>
+                    </tr>
+                   )}
               </thead>
               <tbody>
 
